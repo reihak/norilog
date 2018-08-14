9f186a77490c9c412264591cf3f55bf8eb112bdc
ヘイクス 礼 <reihakes@smokeysmac.local>
2018-08-14 21:17:56 +0900

    revision

--- README.rst
+++ README.rst
@@ -37,8 +37,25 @@ pip 18.0
   開発手順
   ==========
 
-  開発様インストール
-  =================
+依存ライブラリの変更時
+-----------------------
+
+1. ``setup.py`` の ``install_requires`` を更新する
+2. 以下手順で環境を更新する
+
+  (venv) $ deactivate
+
+  $ python 3.6 -m venv --clear venv
+
+  $ source venv/bin/deactivate
+
+  (venv) $ pip install -e ./norilog
+  (venv) $ pip freeze > requirements.txt
+
+3.  setup.pyとrequirements.txtをリボじトリーにコミットする
+
+開発用インストール
+-------------------
 
   1.チェックアウトする
   2.以下手順でインストールする
--- setup.py
+++ setup.py
@@ -1,10 +1,32 @@
+import os
 from setuptools import setup, find_packages
 
+def read_file(filename):
+    basepath = os.path.dirname(os.path.dirname(__file__))
+    filepath = os.path.join(basepath, filename)
+    if os.path.exists(filepath):
+        return open(filepath).read()
+    else:
+        return ''
 setup(
     name='norilog',
     version = '1.0.0',
+    description = 'The NOrilog web application',
+    long_description = read_file('README.rst'),
+    author = '<あなたの名前>',
+    author_email='<あなたのメールアドレス>',
+    url ='https://github.com/<あなたのGithubアカウント>/norilog/',
+    classifiers = [
+        'Development Status :: 4 -Bata',
+        'Framework :: Flask '
+        'License :: OSI approved :: BSD License',
+        'Programing Language :: Python',
+        'Programing Language :: Python :: 3.6',
+    ],
     packages = find_packages(),
     include_package_data = True,
+    keywords = ['web', 'norilog'],
+    license = 'BSD License',
     install_requires = [
      'Flask',
     ],
